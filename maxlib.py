3882a3063ae38dc4a5041986371f132d1605ef47
A-Julien <julien.alaimo@gmail.com>
2017-03-13 14:41:57 +0100

    building maxLib in python [don't work]

--- maxlib.py
+++ maxlib.py
@@ -12,10 +12,11 @@ import RPi.GPIO as GPIO
 import time
 #     |1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H|NNN|
 
-#---------------------------------------------
+#-----------------------------------------------
 #Take BRUTE order and display it			
-# Frame : |A,F,B,G,E,C,D,2,H,4,6,7,8,5,3,1,9,N,N,N|
-#---------------------------------------------
+# In  :|A,F,B,G,E,C,D,2,H,4,6,7,8,5,3,1,9,N,N,N|
+# Out : Max6921
+#-----------------------------------------------
 def display(tab[],tmpclk,tmpload)
 	for x in range(20):
 		#Data
@@ -33,3 +34,38 @@ def display(tab[],tmpclk,tmpload)
 	time.sleep(tmpload)          # Time in the high state
 	GPIO.output(LOAD, GPIO.LOW)  # Falling edge of LOAD to load all the buffer 
 #---------------------------------------------
+
+#-----------------------------------------------
+# Take Logical Frame and convert in BRUTE frame
+# In  : Digit=Segment,
+# In  : |1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H|
+# Out : |A,F,B,G,E,C,D,2,H,4,6,7,8,5,3,1,9,N,N,N|
+#-----------------------------------------------
+def assoc(*args):
+	for arg in args:
+		
+
+
+	for x in xrange(0,16):
+		tab[x]
+	return _tab[]
+
+#-------------------------------------------------
+# Take ASCII code, return code of it in logical frame
+# In  : Segment,Segment,..
+# In  : A,1,D,...
+# Out : |0,0,0,0,0,0,0,0,0,A,B,C,D,E,F,G,H|
+#-------------------------------------------------
+def segment($args):
+	for arg in args:
+
+
+
+
+
+
+
+
+
+
+	
